74bc2bec622c83a33e95c2a74c86e463dabb5cff
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-02-14 08:44:26 -0700

    added functions to query db

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # playlist-mysql
+
+Created mysql schema to populate database 
+when you run javascript in node, methods query all songs and specific
--- playlistRead.js
+++ playlistRead.js
@@ -1,7 +1,7 @@
 let mysql = require("mysql");
 let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+  host: "localhost",
+  port: 3306,
   user: "root",
   password: "password",
   database: "playlist_db"
@@ -10,14 +10,67 @@ let connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
 
-
   console.log("connected as id: " + connection.threadId);
+  queryAllSongs();
+  queryRapSongs();
+  queryRockSongs();
 });
 
-connection.query("SELECT * FROM songs", function(err, result, fields) {
-    if (err) throw err;
-    console.log(result);
+function queryAllSongs() {
+  connection.query("SELECT * FROM songs", function(err, result) {
+    for (let i = 0; i < result.length; i++) {
+      console.log(
+        result[i].id +
+          " | " +
+          result[i].title +
+          " | " +
+          result[i].artist +
+          " | " +
+          result[i].genre
+      );
+    }
+    console.log("---------------------------------------");
   });
-  {
-    return;
-  }
+}
+
+function queryRapSongs() {
+  let query = connection.query(
+    "SELECT * FROM songs WHERE genre=?",
+    ["Rap"],
+    function(err, result) {
+      for (let i = 0; i < result.length; i++) {
+        console.log(
+          result[i].id +
+            " | " +
+            result[i].title +
+            " | " +
+            result[i].artist +
+            " | " +
+            result[i].genre
+        );
+      }
+      console.log(query.sql);
+    }
+  );
+}
+
+function queryRockSongs() {
+  let query = connection.query(
+    "SELECT * FROM songs WHERE genre=?",
+    ["Rock"],
+    function(err, result) {
+      for (let i = 0; i < result.length; i++) {
+        console.log(
+          result[i].id +
+            " | " +
+            result[i].title +
+            " | " +
+            result[i].artist +
+            " | " +
+            result[i].genre
+        );
+      }
+      console.log(query.sql);
+    }
+  );
+}
