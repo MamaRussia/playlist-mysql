4810a8a004f276abef06a06067e99fd6179f3b22
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-02-13 22:24:56 -0700

    added mysql connection

--- playlistRead.js
+++ playlistRead.js
@@ -1,4 +1,23 @@
 let mysql = require("mysql");
 let connection = mysql.createConnection({
-    host: "localhost:3306"
-})
+    host: "localhost",
+    port: 3306,
+  user: "root",
+  password: "password",
+  database: "playlist_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+
+
+  console.log("connected as id: " + connection.threadId);
+});
+
+connection.query("SELECT * FROM songs", function(err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+  {
+    return;
+  }
